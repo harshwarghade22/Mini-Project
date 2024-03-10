1f9c83cd6f3516a6a9d47f55b207c53bf1dc3b72
shivamm <sm0011932@gmail.com>
2024-03-10 19:45:59 +0530

    fix: navbar responsivness

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -53,18 +53,15 @@ function Filters() {
   };
 
   return (
-    <>
-      <div className="w-full h-[4.94vw] bg-[#FFFFFF] text-center text-3xl ">
-        Nav bar
-      </div>
-
-      <div className="search w-full h-[5.46vw] bg-[#FFFFFF]  flex justify-between items-center pl-20 mb-4 border-t-[1px] pt-2 border-black  ">
-        <h1 className="text-3xl font-['Gilroy-Medium'] text-zinc-800 font-medium tracking-tight">
-          Mumbai,
-        </h1>
-        <div className="left w-[65%] h-full  flex justify-center items-center  ">
+    <div  className="">
+      
+      <div className="search w-11/12 mx-auto bg-[#FFFFFF]  flex flex-col xl:flex-row item-start lg:items-center justify-evenly space-y-4 xl:space-y-0 border-t-[1px] pt-2 border-black  ">
+        <div className="left h-full flex justify-start lg:justify-center items-start gap-4 md:gap-8">
+          <h1 className="text-3xl text-start font-['Gilroy-Medium'] text-zinc-800 font-medium tracking-tight">
+            Mumbai,
+          </h1>
           <Multiselect
-            className="w-full h-full ml-2 "
+            className="w-[300px] max-w-xs h-full"
             isObject={false}
             options={Location}
             onSelect={handleLocationChange}
@@ -72,11 +69,11 @@ function Filters() {
             showCheckbox
           />
 
-          <div className="price ml-12 flex justify-center items-center border-zinc-600 border-opacity-50 border-[1px] rounded-full ">
+          <div className="price flex justify-center items-center border-zinc-600 border-opacity-50 border-[1px] rounded-full ">
             <select
               value={priceValue}
               onChange={priceChangeHandler}
-              className="py-3 w-40 px-4 rounded-full z-10 hover:scale-105 hover:bg-gradient-to-t from-zinc-300 ease-in-out duration-700 "
+              className="py-3  px-4 rounded-full z-10 hover:scale-105 hover:bg-gradient-to-t from-zinc-300 ease-in-out duration-700 "
             >
               <option value="">Select Price</option>
               <option value="2000 - 3000">2000 - 3000</option>
@@ -89,8 +86,8 @@ function Filters() {
           </div>
         </div>
 
-        <div className="right w-[38%] h-full  flex justify-center items-center   mr-16  ">
-          <div className="w-full flex justify-center items-center gap-6  rounded-full shadow-lg  p-1  ">
+        <div className="right h-full  flex justify-start lg:justify-center items-start gap-4 md:gap-8">
+          <div className="min-w-fit w-full flex justify-center items-center gap-6  rounded-full shadow-lg  p-1  ">
             <h1 className="text-xl font-['Gilroy-Medium'] tracking-tight ">
               Mumbai
             </h1>
@@ -104,7 +101,7 @@ function Filters() {
               <FilterArrow />
             </button>
           </div>
-          <button className="bg-[#F45C2C] py-2 px-6 rounded-full flex justify-center items-center gap-2  text-white text-xl font-['Gilroy-Medium'] ml-4">
+          <button className="bg-[#F45C2C] py-2 px-6 rounded-full flex justify-center items-center gap-2  text-white text-xl font-['Gilroy-Medium']">
             Go <Arrow />
           </button>
         </div>
@@ -113,7 +110,7 @@ function Filters() {
       <div>
         <h1 className="text-[#F45C2C] font-semibold text-2xl  px-20 opacity-85">
           <span className="text-zinc-700 opacity-50">Spaces near </span>
-          "Metro Station"
+          Metro Station
         </h1>
 
         <div className=" card w-full  border-1 py-10  px-4">
@@ -124,7 +121,7 @@ function Filters() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
