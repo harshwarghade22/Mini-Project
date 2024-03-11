acbb0dcc86aa66d090a3a3e5bf3eea198c4c6ad7
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-03-12 02:12:34 +0530

    date function added

--- src/components/Reserve2.jsx
+++ src/components/Reserve2.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { IoBed } from "react-icons/io5";
 import { FaArrowRight } from "react-icons/fa";
 import { BsCalendarDateFill } from "react-icons/bs";
 
 
+
 function Reserve2() {
+  // State to manage the current date
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [dateFinal,setDateFinal] = useState(new Date());
+
+  // Function to handle date changes
+  const handleDateChange = (event) => {
+    const selectedDate = new Date(event.target.value);
+    setCurrentDate(selectedDate);
+    setDateFinal(selectedDate);
+  };
+
+
   return (
     <div>
       <div className='h-[600px] mr-[100px] ml-[100px] mt-5  flex gap-48'>
@@ -53,42 +66,52 @@ function Reserve2() {
                 </div>
             </div>
         </div>
-        <div className='w-[550px]  flex flex-col'>
-            <div className='h-[75px]  flex justify-between'>
-                <div className='w-[100px] h-full  flex flex-col justify-center items-center gap-2'>
-                    <p className='font-gilroy_semi_bold text-center text-sm'>Select pricing</p>
-                    <div className='w-[45px] h-[45px] rounded-full border-black border flex justify-center items-center text-2xl bg-black text-white'>
-                        <IoBed/>
-                    </div>
+        <div className='w-[450px]  flex flex-col'>
+        <div className='h-[75px]  flex justify-between'>
+            <div className='w-[100px] h-full  flex flex-col justify-center items-center gap-2'>
+                <p className='font-gilroy_semi_bold text-center text-sm'>Select pricing</p>
+                <a href="/reserve1">
+                <div className='w-[45px] h-[45px] rounded-full border-black border flex justify-center items-center text-2xl bg-black text-white'>
+                    <IoBed/>
                 </div>
-                <div className='w-[100px] h-full  flex flex-col justify-center items-center gap-2'>
-                    <p className='font-gilroy_semi_bold text-center text-sm text-gray-400'>Select date</p>
-                    <div className='w-[45px] h-[45px] rounded-full border-black border flex justify-center items-center text-2xl bg-black text-white'>
-                        <BsCalendarDateFill/>
-                    </div>
-                </div>
-                <div className='w-[100px] h-full  flex flex-col justify-center items-center gap-2'>
-                    <p className='font-gilroy_semi_bold text-center text-sm text-gray-400'>Confirm & Pay</p>
-                    <div className='w-[45px] h-[45px] rounded-full border-black border flex'></div>
+                </a>
+               
+            </div>
+            <div className='w-[100px] h-full  flex flex-col justify-center items-center gap-2'>
+                <p className='font-gilroy_semi_bold text-center text-sm text-gray-400'>Select date</p>
+                <div className='w-[45px] h-[45px] rounded-full border-black border flex justify-center items-center text-2xl bg-black text-white'>
+                    <BsCalendarDateFill/>
                 </div>
             </div>
+            <div className='w-[100px] h-full  flex flex-col justify-center items-center gap-2'>
+                <p className='font-gilroy_semi_bold text-center text-sm text-gray-400'>Confirm & Pay</p>
+                <div className='w-[45px] h-[45px] rounded-full border-black border flex'></div>
+            </div>
+        </div>
 
-            <div className='mt-16'>
-                <p className='font-gilroy_medium'>Select Date</p>
-                <div className='h-[50px] bg-slate-200 rounded-lg mt-4'>
-                    
-                </div>
+        <div className='container mx-auto px-4 py-8'>
+            <div className="mb-4">
+                <label htmlFor="date" className="block text-gray-700 font-bold mb-2">Select a date:</label>
+                <input
+                type="date"
+                id="date"
+                name="date"
+                value={currentDate.toISOString().slice(0, 10)}
+                onChange={handleDateChange}
+                className=" px-4 py-2 rounded-xl w-full font-gilroy_semi_bold bg-slate-100"
+                />
             </div>
+        </div>
             
             
-            <div className='h-[100px] border-t border-t-gray-400 mt-64 flex justify-between p-5'>
+            <div className='h-[100px] border-t border-t-gray-400 mt-44 flex justify-between p-5'>
                 <div className='flex gap-2'>
                     
                     <div className='w-[130px] h-[50px] rounded-full bg-slate-200 text-center pt-3 font-gilroy_regular'>
-                    ₹7000/mo
+                    70000
                     </div>
                     <div className='w-[130px] h-[50px] rounded-full bg-slate-200 text-center pt-3 font-gilroy_semi_bold'>
-                    09/03/2024
+                    {dateFinal.getDate()}/{dateFinal.getMonth()+1}/{dateFinal.getFullYear()}
                     </div>
 
                 </div>
