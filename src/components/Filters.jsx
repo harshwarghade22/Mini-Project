79e25edcf1def9b6076f42434f099844ea603008
React-projects <avishwakarma8855@gmail.com>
2024-03-16 23:22:08 +0530

    filters page update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -44,7 +44,7 @@ function Hero() {
   };
 
   return (
-    <div className="py-8 px-4 ">
+    <div className="py-4 px-4 ">
       <div className="max-w-7xl mx-auto grid lg:grid-cols-12  ">
         <div className="col-span-1 pt-2">
           <h1 className='sm:textlg lg:text-xl md:text-2xl font-medium font-["Gilroy Medium"] mt-6'>
@@ -52,32 +52,8 @@ function Hero() {
           </h1>
         </div>
         <div className="col-span-7  sm:flex justify-start items-center gap-4 my-6 sm:px-4">
-          {/* <div className="border-[1px] border-black/40  ">
-            <Multiselect
-              className="   col-span-1 bg-[#F3F5F7]"
-              isObject={false}
-              options={Location}
-              toggelOptionList={true}
-              onSelect={handleLocationChange}
-              onRemove={handleLocationChange}
-              showCheckbox
-            />
-          </div> */}
           <LocationSelect />
           <PriceSelect />
-          {/* <select
-            value={priceValue}
-            onChange={priceChangeHandler}
-            className="py-3 w-40 px-4 border-[1px] border-black/50 z-10 ease-in-out duration-700 sm:mt-0  rounded-full  my-4 sm:my-0"
-          >
-            <option value="">Select Price</option>
-            <option value="2000 - 3000">2000 - 3000</option>
-            <option value="3500 - 5000">3500 - 5000</option>
-            <option value="5500 - 7000">5500 - 7000</option>
-            <option value="7500 - 9000">7500 - 9000</option>
-            <option value="9500 - 12000">9500 - 12000</option>
-            <option value="12500 - 15000">12500 - 15000</option>
-          </select> */}
         </div>
 
         <div className="col-span-4 flex justify-between items-center lg:ml-4 ">
@@ -102,7 +78,7 @@ function Hero() {
       </div>
 
       <div className="max-w-7xl mx-auto ">
-        <h1 className="text-[#F45C2C] font-semibold text-2xl  opacity-85 my-6">
+        <h1 className="text-orange-400 font-semibold text-2xl my-4">
           <span className="text-zinc-700 opacity-50">Spaces near </span>
           "Metro Station"
         </h1>
