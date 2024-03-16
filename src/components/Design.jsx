681e3095289d431e64250846f6b26768c0d6cbe6
Sohamat <sohamvichare8@gmail.com>
2024-03-16 14:07:10 +0530

    responsive home

---
+++ src/components/Design.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function Design() {
+  return (
+    <div className="bg-zinc-50 row-span-2 flex justify-evenly relative  ">
+          <div className='absolute top-1/3 p-3'><h1 className='font-bold text-2xl md:text-3xl'>The ideal search for flat share ends here </h1>
+          <p className='text-xl md:text-2xl'>with more than 100k flats on rent</p></div>
+
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+            <div className="h-full w-[2%] bg-zinc-200"></div>
+
+
+
+        </div>
+  )
+}
+
+export default Design
