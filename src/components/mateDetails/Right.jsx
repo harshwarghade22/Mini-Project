b34875961c3409601e6567e27c7632a731dc6ced
React-projects <avishwakarma8855@gmail.com>
2024-03-19 22:06:57 +0530

    icons page organized

--- src/components/mateDetails/Right.jsx
+++ src/components/mateDetails/Right.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import home1 from "../Images/Rectangle 93.png";
-import location from "../Images/Vector.png";
-import rect106 from "../Images/Rectangle 106.png";
-import rect2896 from "../Images/Rectangle 2896.png";
-import rect2895 from "../Images/Rectangle 2895.png";
-import rect2894 from "../Images/Rectangle 2894.png";
-import fridge from "../Images/Isolation_Mode.png";
-import layer1 from "../Images/Layer_1.png";
-import icon3 from "../Images/Group 48096360.png";
-import icon4 from "../Images/Layer_1 (1).png";
-import icon5 from "../Images/Layer_1 (2).png";
-import icon6 from "../Images/Layer_1 (3).png";
-import icon7 from "../Images/Group 48096361 (1).png";
-import icon8 from "../Images/Layer_1 (5).png";
-import icon9 from "../Images/548-Bed, Hotel, Single, Sleep, Frame.png";
-import icon10 from "../Images/Group 48096362img10.png";
+import {
+  home1,
+  location,
+  rect106,
+  rect2896,
+  rect2895,
+  rect2894,
+  fridge,
+  layer1,
+  icon3,
+  icon4,
+  icon5,
+  icon6,
+  icon7,
+  icon8,
+  icon9,
+  icon10,
+} from "./Icons";
+
 import { GoHeart } from "react-icons/go";
 import { IoMdCall } from "react-icons/io";
 
