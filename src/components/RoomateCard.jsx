a5932388fdc9028a0c85b4026ad91c1a5284e2f3
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-03-26 17:32:49 +0530

    Roomate Cards updated

--- src/components/RoomateCard.jsx
+++ src/components/RoomateCard.jsx
@@ -5,29 +5,36 @@ import Fav from "./Icons/Fav";
 function RoomateCard() {
   return (
     <div>
-        <div className='w-full flex flex-col border rounded-lg p-3 font-gilroy_light hover:scale-105 duration-300'>
+      <div className="w-[90%] mx-auto md:mx-0 flex flex-col border rounded-lg p-3 font-gilroy_light">
+        <div className="w-full md:h-[40vh] h-[40vh]  border rounded-lg">
+          <img
+            src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className="object-scale-down  h-full w-full rounded-lg"
+          />
+        </div>
 
-          <div className='w-full md:h-[40vh] h-[70vh]  border rounded-lg'>
-            <img src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className='object-cover object-center  h-full w-full rounded-lg' />
+        <div className="pt-3">
+          <p className="text-sm">Working professional</p>
+          <h1 className="text-xl font-gilroy_bold">Emma F.</h1>
+        </div>
+        <div className="flex justify-between h-[15%] mt-3">
+          <div className="flex flex-col h-full justify-between">
+            <p className="text-sm">Budget</p>
+            <p>8000Rs</p>
           </div>
-          
-          <div className='pt-3'>
-            <p className='text-sm'>Working professional</p>
-            <h1 className='text-xl font-gilroy_bold'>Emma F.</h1>
+          <div className="flex flex-col h-full justify-between">
+            <p className="text-sm">Looking for</p>
+            <p>Female</p>
           </div>
-          <div className='flex justify-between h-[15%] mt-3'>
-            <div className='flex flex-col h-full justify-between'>
-              <p className='text-sm'>Budget</p>
-              <p>8000Rs</p>
-            </div>
-            <div className='flex flex-col h-full justify-between'>
-              <p className='text-sm'>Looking for</p>
-              <p>Female</p>
-            </div>
-            <div className='flex flex-col h-full justify-between'>
-              <p className='text-sm'>Looking for</p>
-              <p>Flatmate</p>
-            </div>
+          <div className="flex flex-col h-full justify-between">
+            <p className="text-sm">Looking for</p>
+            <p>Flatmate</p>
+          </div>
+        </div>
+        <div className="w-full h-[13%] mt-3 border-t pt-3 flex gap-2">
+          <div className="w-[25%] border-black border rounded-lg flex justify-center items-center ">
+            <Fav />
           </div>
           <div className="w-[25%] border-black border rounded-lg flex justify-center items-center">
             <Contact />
@@ -37,7 +44,7 @@ function RoomateCard() {
           </div>
         </div>
       </div>
-    
+    </div>
   );
 }
 
