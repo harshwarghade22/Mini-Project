50ec247f43bd03e23c398c9499a7fb953e154ceb
GauravGalbal <129824261+GauravGalbal@users.noreply.github.com>
2024-04-06 14:40:45 +0530

    Customer & Workman Case Progress sync

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { data } from "../data.js";
 import { useState } from "react";
 import Card from "./Card.jsx";
@@ -7,70 +7,101 @@ import PriceSelect from "./price-select/PriceSelect.jsx";
 import FilterBtn from "./filter-btn/FilterBtn.jsx";
 import { Link } from "react-router-dom";
 
+import { getAllFlats } from "../serverAPI/flats/api.js";
+import axios from "axios";
+
 function Hero() {
-  const [listedProperties, setListedProperties] = useState(data);
-  const [filterData, setFilterData] = useState(data);
-  const [priceValue, setPriceValue] = useState("");
-  const [Location, setLocation] = useState([
-    "Mumbai",
-    "Andheri",
-    "Mira-Road",
-    "Borivali",
-  ]);
+    const serverURL = import.meta.env.VITE_SERVER_URL
+    const readAuthToken = import.meta.env.VITE_STRAPI_READ_AUTH_TOKEN
 
-  const handleLocationChange = (selectedLocations) => {
-    const searchProperties = listedProperties.filter((property) =>
-      selectedLocations.some((location) =>
-        property.location.toLowerCase().includes(location.toLowerCase())
-      )
-    );
-    setFilterData(searchProperties);
-  };
+    const [listedProperties, setListedProperties] = useState(data);
+    const [filterData, setFilterData] = useState(data);
+    const [priceValue, setPriceValue] = useState("");
+    const [Location, setLocation] = useState([
+        "Mumbai",
+        "Andheri",
+        "Mira-Road",
+        "Borivali",
+    ]);
 
-  const priceChangeHandler = (e) => {
-    const selectedRange = e.target.value;
-    setPriceValue(selectedRange);
+    useEffect(() => {
+        async function fetchFlats() {
+            try {
+                let config = {
+                    method: 'get',
+                    maxBodyLength: Infinity,
+                    url: `${serverURL}/${getAllFlats}`,
+                    headers: {
+                        'Authorization': readAuthToken
+                    }
+                };
 
-    if (selectedRange) {
-      const [min, max] = selectedRange.split("-").map(Number);
-      const priceFilter = listedProperties.filter(
-        (item) => item.price >= min && item.price <= max
-      );
-      setFilterData(priceFilter);
-    } else {
-      setFilterData(listedProperties);
-    }
-  };
+                const res = await axios.request(config)
 
-  return (
-    <div className="py-4 px-4 ">
-      <div className="max-w-7xl mx-auto grid md:grid-cols-12">
-        <div className="xl:col-span-1 col-span-1 my-4 lg:px-2 px-4">
-          <h1 className="lg:text-xl md:text-2xl text-2xl font-medium font-gilroy_medium">
-            Mumbai,
-          </h1>
-        </div>
+                console.log(res.data)
 
-        <div className="xl:col-span-11  md:col-span-12 px-4 lg:flex lg:justify-between justify-start ">
-          <div className="my-4">
-            <LocationSelect />
-          </div>
-          <div className="my-4">
-            <PriceSelect />
-          </div>
-          <div className="my-4">
-            {" "}
-            <FilterBtn />
-          </div>
-        </div>
+            } catch (err) {
+                console.log(err)
+            }
+        }
 
-        {/* <div className="xl:col-span-1 md:col-span-4 xl:p-4 py-10 px-4">
+        fetchFlats();
+    })
+
+
+    const handleLocationChange = (selectedLocations) => {
+        const searchProperties = listedProperties.filter((property) =>
+            selectedLocations.some((location) =>
+                property.location.toLowerCase().includes(location.toLowerCase())
+            )
+        );
+        setFilterData(searchProperties);
+    };
+
+    const priceChangeHandler = (e) => {
+        const selectedRange = e.target.value;
+        setPriceValue(selectedRange);
+
+        if (selectedRange) {
+            const [min, max] = selectedRange.split("-").map(Number);
+            const priceFilter = listedProperties.filter(
+                (item) => item.price >= min && item.price <= max
+            );
+            setFilterData(priceFilter);
+        } else {
+            setFilterData(listedProperties);
+        }
+    };
+
+    return (
+        <div className="py-4 px-4 ">
+            <div className="max-w-7xl mx-auto grid md:grid-cols-12">
+                <div className="xl:col-span-1 col-span-1 my-4 lg:px-2 px-4">
+                    <h1 className="lg:text-xl md:text-2xl text-2xl font-medium font-gilroy_medium">
+                        Mumbai,
+                    </h1>
+                </div>
+
+                <div className="xl:col-span-11  md:col-span-12 px-4 lg:flex lg:justify-between justify-start ">
+                    <div className="my-4">
+                        <LocationSelect />
+                    </div>
+                    <div className="my-4">
+                        <PriceSelect />
+                    </div>
+                    <div className="my-4">
+                        {" "}
+                        <FilterBtn />
+                    </div>
+                </div>
+
+                {/* <div className="xl:col-span-1 md:col-span-4 xl:p-4 py-10 px-4">
           <button className="py-2 px-4  rounded-full bg-[#F45C2C] text-white font-medium  flex justify-between items-center">
             Go <Arrow />
           </button>
         </div> */}
 
-        {/* <div className="xl:col-span-2 md:col-span-6 lg:px-0">
+                {/* <div className="xl:col-span-2 md:col-span-6 lg:px-0">
           <div className="flex justify-between items-center  md:my-4  shadow-xl rounded-full xl:p-2 sm:px-8 px-6 py-4 ">
             <div className=" ">
               <h1 className="xl:text-base lg:text-xl md:text-2xl sm:text-2xl text-2xl  text-center  font-['Gilroy-Medium'] tracking-tight ">
@@ -89,24 +120,24 @@ function Hero() {
             </div>
           </div>
         </div> */}
-      </div>
+            </div>
 
-      <div className="max-w-7xl mx-auto xl:px-4 px-2 ">
-        <h1 className="text-borderGray font-semibold text-2xl my-4">
-          <span className="">Spaces near </span>
-          "Metro Station"
-        </h1>
-      </div>
+            <div className="max-w-7xl mx-auto xl:px-4 px-2 ">
+                <h1 className="text-borderGray font-semibold text-2xl my-4">
+                    <span className="">Spaces near </span>
+                    "Metro Station"
+                </h1>
+            </div>
 
-      <div className="max-w-7xl mx-auto  xl:grid-cols-3 lg:grid-cols-2 grid gap-8 mt-4">
-        {filterData.map((item, index) => (
-          <Link to="/details">
-            <Card key={item.id} data={item} />
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="max-w-7xl mx-auto  xl:grid-cols-3 lg:grid-cols-2 grid gap-8 mt-4">
+                {filterData.map((item, index) => (
+                    <Link to="/details">
+                        <Card key={item.id} data={item} />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
---
+++ src/serverAPI/flats/api.js
@@ -0,0 +1,3 @@
+export const getFlatsByLocationPrice = "flats?filters[$and][0][city][$eqi]=Mumbai&filters[$and][1][rent][$between]=5001&filters[$and][1][rent][$between]=6000&populate=*"
+
+export const getAllFlats = "flats?populate=*"
