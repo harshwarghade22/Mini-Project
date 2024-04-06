4a9738111679099dcb176d4d96c538d38b414914
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-04-07 00:58:15 +0530

    roomate card

--- src/components/RoomateCard.jsx
+++ src/components/RoomateCard.jsx
@@ -7,7 +7,7 @@ function RoomateCard() {
   
   return (
     <div>
-      <div className="w-[90%] md:w-[100%] mx-auto flex flex-col border rounded-3xl p-3 font-gilroy_light shadow-xl">
+      <div className="w-[90%] md:w-[100%] mx-auto flex flex-col border rounded-3xl p-2 font-gilroy_light shadow-xl">
         <div className="w-full md:h-[35vh] h-[40vh] border rounded-3xl">
           <img
             src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
