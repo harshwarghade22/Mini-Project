305324088af1f2dd1e584405ff845ea36db77983
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-04-07 00:28:13 +0530

    Location pages responsive

--- src/components/RoomateCard.jsx
+++ src/components/RoomateCard.jsx
@@ -12,7 +12,7 @@ function RoomateCard() {
           <img
             src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
-            className="object-scale-down h-full w-full rounded-lg"
+            className="object-fill object-center h-full w-full rounded-lg"
           />
         </div>
 
