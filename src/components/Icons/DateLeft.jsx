09968b747dae55e979ccd63303cc8db591fb1234
GauravGalbal <129824261+GauravGalbal@users.noreply.github.com>
2024-04-20 14:19:51 +0530

    react query updates

--- src/components/Icons/DateLeft.jsx
+++ src/components/Icons/DateLeft.jsx
@@ -18,13 +18,13 @@ function DateLeft() {
           rx="11.2877"
           transform="matrix(4.37114e-08 -1 -1 -4.37114e-08 22.5755 22.5755)"
           stroke="#434343"
-          stroke-width="0.42454"
+          strokeWidth="0.42454"
         />
         <path
           d="M12.7163 14.5576L9.40137 11.2427L11.0588 9.58521L12.7163 7.92774"
           stroke="#434343"
-          stroke-width="0.736653"
-          stroke-linecap="round"
+          strokeWidth="0.736653"
+          strokeLinecap="round"
         />
       </svg>
     </div>
