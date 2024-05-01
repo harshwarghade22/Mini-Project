4ad3d20f4cfabb1761b4097dd050a418cf5f7018
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-05-01 14:27:06 +0530

    authentication

--- src/components/Onboarding/Google.jsx
+++ src/components/Onboarding/Google.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 
 function GoogleAuthentication() {
   return (
     <div className='text-center pt-5 '>
-        <GoogleLogin
-  onSuccess={credentialResponse => {
-    var credentialResponseDecoded = jwtDecode(credentialResponse.credential);
-    console.log(credentialResponseDecoded);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>
+      <GoogleOAuthProvider >
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          var credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+          console.log(credentialResponseDecoded);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+      </GoogleOAuthProvider>
+      
     </div>
   )
 }
